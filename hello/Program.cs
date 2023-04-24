b7874f761f2bd854702bcfbd72e754d2dc6c5363
unknown <sesnadia03@gmail.com>
2023-04-24 22:30:54 +0700

    комментарий к коммиту

---
+++ hello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello woorld");
+            Console.WriteLine("Hi, Oh, Mark");
+            Console.ReadKey();
+        }
+    }
+}
